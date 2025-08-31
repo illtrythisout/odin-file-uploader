d85d8c2b55e4b329a4f18d91df9ada92cf6334e3
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-08-31 09:35:38 +0200

    fix: implement protected route
    This way only authenticated users can access a dashboard

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const router = Router();
+const protectRoute = require('../middleware/authMiddleware');
 
 const indexController = require('../controllers/indexController');
 const passport = require('passport');
 
 // drive routes
 const userRouter = require('./userRouter');
-router.use('/drive', userRouter);
+router.use('/drive', protectRoute.protectRoute, userRouter);
 
 // index routes
 
